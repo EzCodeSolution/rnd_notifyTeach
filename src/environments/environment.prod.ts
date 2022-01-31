c68e7eafdcdd508ef77f537af09b0cadfc2f22c4
Thanaha <thanaha@thanahas-MacBook-Pro.local>
2022-01-31 08:20:11 +0700

    change url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  lineNotiUrl : 'http://localhost:3000',
+  lineNotiUrl : 'https://rndteachapi.herokuapp.com',
   lineApiKey : 'PCliK3G0JimYbPVFsakd8ECt3F8pWfQGLOIT1zqQqnT',
 };
